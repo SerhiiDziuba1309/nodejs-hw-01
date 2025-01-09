3349cc4e5f5dbf8ecfa1aad4cacd35f0390cf5e0
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2025-01-09 15:53:17 +0100

    add seventh step

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,14 @@
-export const removeAllContacts = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    const deleteContacts = [];
+    await writeContacts(deleteContacts);
+    console.log('All contacts successfully deleted.');
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error;
+  }
+};
 
 removeAllContacts();
