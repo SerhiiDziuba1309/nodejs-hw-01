baf443fedae0310569661055434763d23376f01e
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2025-01-09 15:10:20 +0100

    add third foot

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+    const newContacts = Array.from({ length: number }, createFakeContact);
+    const updatedContacts = [...existingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+    console.log(`${number} new contacts added.`);
+  } catch (error) {
+    console.error('Error generation:', error.message);
+  }
+};
 
 generateContacts(5);
