b28146a65e5324a42ae4ecb1609539308ad950c6
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2025-01-09 15:29:23 +0100

    add fourth step

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,16 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    const newContact = createFakeContact();
+    const updatedContacts = existingContacts.concat(newContact);
+    await writeContacts(updatedContacts);
+    console.log('One contact successfully added:', newContact);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+};
 
 addOneContact();
