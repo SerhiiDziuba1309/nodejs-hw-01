9cdd8931b1594a29aa77881f6deb29f548809afe
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2025-01-09 12:14:28 +0100

    add first foot contacts

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const currentFile = fileURLToPath(import.meta.url);
+const currentDir = path.dirname(currentFile);
+export const PATH_DB = path.join(currentDir, '../db/db.json');
